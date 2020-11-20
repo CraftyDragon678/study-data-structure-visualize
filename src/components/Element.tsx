fe3d334daf4cde08389809b91df3e546fd635ac8
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-20 22:21:12 +0900

    fix: element style

--- src/components/Element.tsx
+++ src/components/Element.tsx
@@ -2,8 +2,12 @@ import styled from "@emotion/styled";
 import variables from "../styles/variables";
 
 const Element = styled.div`
-  width: 300px;
-  height: 100px;
+  width: 150px;
+  height: 50px;
+  border: 1px solid black;
+  box-sizing: border-box;
+  display: grid;
+  place-items: center;
 `;
 
 export default Element;
